7a39ffc72b959249829d284ac0e6aacf3f22faf3
melyoda <amv123doom@gmail.com>
2025-08-31 16:07:16 +0300

    creating new book document

--- src/main/java/com/bookhub/api/model/Book.java
+++ src/main/java/com/bookhub/api/model/Book.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "books")
 public class Book {
 
     @Id
